30a9a371f442d94987eb47073c9599dfea89da63
Akshay-raveedran <akshayraveendran1999@gmail.com>
2021-10-15 13:40:18 +0530

    add cart controller

---
+++ WebApplication3/Models/ICartRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    interface ICartRepository
+    {
+        List<Book> GetCartById(int userId);
+
+        bool addBookToCartById(int userId, int bookId);
+
+        bool RemoveFromCartById(int userId, int bookId);
+        bool RemoveCartofUser(int userId);
+    }
+}
