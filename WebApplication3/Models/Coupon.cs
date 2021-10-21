ca5ecaa007d9fc63114ebae8e74521ad1606944f
alangthomas <alangthomas4@gmail.com>
2021-10-21 12:51:06 +0530

    added coupon controller

---
+++ WebApplication3/Models/Coupon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Coupon
+    {
+        public int CouponID { get; set; }
+        public int Discount { get; set; }
+        public int MaxAmount { get; set; }
+        public string CouponCode { get; set; }
+    }
+}
